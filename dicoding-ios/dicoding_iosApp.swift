12f320cf984b7e2d9c1b7eb4b5d7ae357b0f169f
Chandra Abdul Fattah <chandrashibezzo@gmail.com>
2021-08-03 21:57:08 +0700

    Initial Commit

---
+++ dicoding-ios/dicoding_iosApp.swift
@@ -0,0 +1,20 @@
+//
+//  dicoding_iosApp.swift
+//  dicoding-ios
+//
+//  Created by Chandra Abdul Fattah on 03/08/21.
+//
+
+import SwiftUI
+
+@main
+struct dicoding_iosApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
